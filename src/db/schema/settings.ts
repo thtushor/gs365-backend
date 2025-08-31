5806be47d9c188f6eefd20a5767c8a8fc766e39e
thtushor <hasankhanltd@gmail.com>
2025-08-31 12:42:39 +0600

    Add maxWithdrawBalance field to settings schema
    - Introduced maxWithdrawBalance field in the settings schema with a default value of 25000, enhancing the configuration options for withdrawal limits.

--- src/db/schema/settings.ts
+++ src/db/schema/settings.ts
@@ -5,6 +5,7 @@ export const settings = mysqlTable("settings", {
   id: int("id").primaryKey().autoincrement(),
   defaultTurnover: int("default_turnover").notNull(),
   adminBalance: decimal("adminBalance").notNull(),
+  maxWithdrawBalance: decimal("max_withdrawable_balance").default("25000"),
   createdAt: datetime("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: datetime("updated_at").default(
     sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`
